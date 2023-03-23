be9dfc9f89a0c4dcaedd07020cdf8b6fba0bb6f0
Owall <97934878+MayOwall@users.noreply.github.com>
2023-03-23 17:23:28 +0900

    Part1

--- todoapp/server.js
+++ todoapp/server.js
@@ -1,20 +1,49 @@
+// env
+require("dotenv/config");
+const { MONGODB_ID, MONGODB_PASSWORD } = process.env;
+
+// server
+const mongoClient = require("mongodb").MongoClient;
 const express = require("express");
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(8080, function () {
-  console.log("hello, 8080");
-});
+let db;
+mongoClient.connect(
+  `mongodb+srv://${MONGODB_ID}:${MONGODB_PASSWORD}@cluster0.sjgsexl.mongodb.net/todoapp?retryWrites=true&w=majority`,
+  (err, client) => {
+    db = client.db("todoapp");
 
-app.get("/", function (req, res) {
-  console.log("hello, main");
+    app.listen(8080, () => {
+      console.log("hello, 8080");
+    });
+  }
+);
+
+// get
+app.get("/", (req, res) => {
+  console.log("👀 Page Access Detected : main");
   res.sendFile(__dirname + "/index.html");
 });
-app.get("/pet", function (req, res) {
-  console.log("hello, pet");
-  res.send("펫, 반갑슴다");
+app.get("/write", (req, res) => {
+  console.log("👀 Page Access Detected : write");
+  res.sendFile(__dirname + "/write.html");
 });
 
-app.get("/beauty", function (req, res) {
-  console.log("hello, beauty");
-  res.send("뷰티, 반갑슴다");
+// post
+app.post("/add", (req, res) => {
+  const { title, date } = req.body;
+  if (!!title && !!date) {
+    const nextData = {
+      title,
+      date,
+    };
+    db.collection("post").insertOne(nextData, () => {
+      console.log("DB:Post 저장 완료");
+    });
+    res.send("전송 완료");
+  } else {
+    console.log("/add post의 title 혹은 date값이 비어있습니다");
+    res.send("에러 발생");
+  }
 });
